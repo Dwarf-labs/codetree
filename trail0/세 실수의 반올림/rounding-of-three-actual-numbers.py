200d414f73eaf2a4d3ea1f26d6238346969c13db
LLAMS <csw051025@naver.com>
2026-05-22 21:09:16 +0900

    [Passed] 세 실수의 반올림 (46ms, 16MB)

--- trail0/세 실수의 반올림/rounding-of-three-actual-numbers.py
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.py
@@ -2,6 +2,7 @@ a = float(input())
 b = float(input())
 c = float(input())
 
-print(f"{a:.3f}")
-print(f"{b:.3f}")
-print(f"{c:.3f}")
+numbers = [a, b, c]
+
+for number in numbers:
+    print(f"{number:.3f}")
